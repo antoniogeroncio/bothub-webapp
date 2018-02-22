4f5eaceaeb7e7288a96da8ed104f4452603a7f0c
Mac Giovanni <maclevison@gmail.com>
2018-02-22 16:31:15 -0300

    Add User Nav

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import Profile from '@/components/Profile'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile
     }
   ]
 })
