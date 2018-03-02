349e0d4cd063854ff28a00e24aa151c403414253
Mac Giovanni <maclevison@gmail.com>
2018-03-01 23:20:25 -0300

    Add Comment input

--- src/components/bot_pages/Issues.vue
+++ src/components/bot_pages/Issues.vue
@@ -1,8 +1,11 @@
 <template>
-  <div class="container">
-    <div class="content">
-      <div class="col col-100">
-
+  <div class="content">
+    <div class="container">
+      <div class="columns">
+        <div class="comment">
+          <input type="text" name="" id="" placeholder="Comment">
+          <button class="btn btn-default" type="submit">Publish</button>
+        </div>
       </div>
     </div>
   </div>
@@ -16,4 +19,30 @@ export default {
 
 <style lang="scss" scoped>
 
+@import '../../assets/scss/_colors';
+
+.comment {
+  background-color: $grey;
+  width: 100%;
+  display: flex;
+  padding: 0 ;
+  border-radius: 5px;
+  margin-top: 40px;
+
+  input[type=text] {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    transition: all 0.2s ease-in-out;
+    margin: 2px 0 2px 2px;
+    border-radius: 4px 0 0 4px;
+
+    &:focus {
+      background-color: $bg-grey;
+    }
+  }
+  .btn {
+    margin: 10px;
+  }
+}
 </style>
