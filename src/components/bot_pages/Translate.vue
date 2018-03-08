673bb4aeb3915d31b857abac774f6fa6a9201966
Mac Giovanni <maclevison@gmail.com>
2018-03-08 13:32:29 -0300

    Add chats pie

--- src/components/bot_pages/Translate.vue
+++ src/components/bot_pages/Translate.vue
@@ -3,6 +3,15 @@
     <div class="content">
       <div class="col col-50">
 
+        <div class="pie" data-percent="5"><span class="pie-percent">5%</span></div>
+        <div class="pie" data-percent="10"><span class="pie-percent">10%</span></div>
+        <div class="pie" data-percent="15"><span class="pie-percent">15%</span></div>
+        <div class="pie" data-percent="20"><span class="pie-percent">20%</span></div>
+        <div class="pie" data-percent="25"><span class="pie-percent">25%</span></div>
+        <div class="pie" data-percent="30"><span class="pie-percent">30%</span></div>
+        <div class="pie" data-percent="50"><span class="pie-percent">50%</span></div>
+        <div class="pie" data-percent="60"><span class="pie-percent">60%</span></div>
+
       </div>
     </div>
   </div>
