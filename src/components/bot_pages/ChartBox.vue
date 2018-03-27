6e4d7fbd644e52e34b70ad1fafaf291a85974691
Mac Giovanni <maclevison@gmail.com>
2018-03-27 11:52:08 -0300

    Add ChartBox

---
+++ src/components/bot_pages/ChartBox.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="translate-box">
+    <div class="translate-box__content">
+      <h5 class="translate-box__title">
+        English
+        <span class="translate-box__flag"><img src="../../assets/images/flags/EN.png" alt=""></span>
+      </h5>
+      <span class="translate-box__status">300 sentences to translate</span>
+    </div>
+
+    <div class="translate-box__chart">
+      <div class="pie" data-percent="10"><span class="pie-percent">10%</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChartBox'
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../../assets/scss/colors';
+
+  .translate-box {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: $bg-grey;
+    border-radius: 5px;
+    overflow: hidden;
+    padding: 15px 7%;
+    margin: 15px;
+    width: calc(50% - 30px);
+
+    @media (max-width: 768px) {
+      width: calc(100% - 20px);
+      margin: 10px;
+    }
+  }
+  .translate-box__content {
+
+  }
+  .translate-box__title {
+    margin: 0;
+  }
+  .translate-box__status {
+    color: #8F8F8F;
+  }
+  .translate-box__flag {
+    width: 15px;
+    display: inline-block;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  $pie-size: 70px;
+  $pie-border: 5px;
+
+  .translate-box__chart {
+
+    .pie {
+      width: $pie-size;
+      height: $pie-size;
+      .pie-percent {
+        width: $pie-size - $pie-border * 2;
+        height: $pie-size - $pie-border * 2;
+        top: $pie-border;
+        left: $pie-border;
+        color: #000;
+        background-color: $bg-grey;
+      }
+    }
+  }
+</style>
--- src/components/bot_pages/Translate.vue
+++ src/components/bot_pages/Translate.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="container">
     <div class="content">
-      <div class="col col-50">
-
-        <div class="pie" data-percent="5"><span class="pie-percent">5%</span></div>
-        <div class="pie" data-percent="10"><span class="pie-percent">10%</span></div>
-        <div class="pie" data-percent="15"><span class="pie-percent">15%</span></div>
-        <div class="pie" data-percent="20"><span class="pie-percent">20%</span></div>
-        <div class="pie" data-percent="25"><span class="pie-percent">25%</span></div>
-        <div class="pie" data-percent="30"><span class="pie-percent">30%</span></div>
-        <div class="pie" data-percent="50"><span class="pie-percent">50%</span></div>
-        <div class="pie" data-percent="60"><span class="pie-percent">60%</span></div>
-
+      <div class="translate-list">
+        <chart-box/>
+        <chart-box/>
+        <chart-box/>
+        <chart-box/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ChartBox from './ChartBox'
 export default {
-  name: 'Translate'
+  name: 'Translate',
+  components: {
+    ChartBox
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+  .translate-list {
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+  }
 </style>
