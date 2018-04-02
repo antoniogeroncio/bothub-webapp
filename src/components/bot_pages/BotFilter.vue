2af0a57730e8b55a24989917ad9329fca0e2b001
Mac Giovanni <maclevison@gmail.com>
2018-04-02 12:14:56 -0300

    Add Base to highlight Text

--- src/components/bot_pages/BotFilter.vue
+++ src/components/bot_pages/BotFilter.vue
@@ -3,7 +3,18 @@
       <div class="lists">
         <div class="list-item list-item--active">
           <div class="list-item-content">
-            What are the symptoms of zika virus?
+
+            <div style="width: 100%;">
+              <div style="position: relative;">
+                <input type="text" value="What are the symptoms of zika virus?" placeholder="text" class="ant-input">
+                <div class="hightlight-text">
+                  <span>What are the symptoms of</span>
+                  <span class="entity-select"> zika virus </span>
+                  <span>?</span>
+                </div>
+              </div>
+            </div>
+
           </div>
           <div class="list-item-tags">
             <span class="badge badge--warning">Diseases <a href="#"><i class="material-icons">cancel</i></a></span>
@@ -46,6 +57,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../../assets/scss/_colors';
 .filter-bot {
   width: 100%;
   background-color: #F4F4F4;
@@ -54,7 +66,40 @@ export default {
   border-radius: 0 0 2px 2px;
   flex-direction: column;
 
+  input[type="text"] {
+    padding: 0;
+    height: 34px;
+    border: none;
+  }
+
+  ::selection {
+    background: $primary-color;
+    color: #fff;
+  }
 }
+
+//--------------------
+// Hight Light Text
+//--------------------
+.entity-select {
+  background-color: #F5A623;
+  opacity: 1;
+  border-radius: 10px;
+  color: #FFF;
+}
+
+.hightlight-text {
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: transparent;
+  pointer-events: none;
+  padding: 5px 8px;
+  white-space: pre;
+}
+
+
+//--------------------
 .filter-actions {
   display: flex;
   flex-wrap: wrap;
