59e233ea6a9d0eb9285cf79c8c5db445529993f7
Mac Giovanni <maclevison@gmail.com>
2018-04-02 16:38:37 -0300

    Adjusting text positioning

--- src/components/bot_pages/BotFilter.vue
+++ src/components/bot_pages/BotFilter.vue
@@ -1,52 +1,48 @@
 <template>
   <div class="filter-bot">
-      <div class="lists">
-        <div class="list-item list-item--active">
-          <div class="list-item-content">
+    <div class="lists">
+      <div class="list-items">
 
-            <div style="width: 100%;">
-              <div style="position: relative;">
-                <input type="text" value="What are the symptoms of zika virus?" placeholder="text" class="ant-input">
-                <div class="hightlight-text">
-                  <span>What are the symptoms of</span>
-                  <span class="entity-select"> zika virus </span>
-                  <span>?</span>
-                </div>
-              </div>
-            </div>
-
-          </div>
-          <div class="list-item-tags">
-            <span class="badge badge--warning">Diseases <a href="#"><i class="material-icons">cancel</i></a></span>
+        <div class="filter-bot__text">
+          <input type="text" value="What are the symptoms of zika virus?" placeholder="text" class="ant-input">
+          <div class="hightlight-text">
+            <span>What are the symptoms of</span>
+            <span class="entity-select"> zika virus </span>
+            <span>?</span>
           </div>
         </div>
-      </div>
 
-      <div class="filter-actions">
-        <b-field>
-            <b-select
-                placeholder="Add Intent"
-                expanded>
-                <option value="diagnosis">diagnosis</option>
-                <option value="treatment">treatment</option>
-                <option value="know_symptoms">Know_symptoms</option>
-                <option value="service_points">Service_points</option>
-            </b-select>
-        </b-field>
-
-        <b-field>
-            <b-select
-                placeholder="Add Entity"
-                expanded>
-                <option value="flint">Flint</option>
-                <option value="silver">Silver</option>
-            </b-select>
-        </b-field>
-
-        <div>
-          <button type="" class="btn btn-secondary">Publish</button>
+        <div class="list-item-tags">
+          <span class="badge badge--warning">Diseases <a href="#"><i class="material-icons">cancel</i></a></span>
         </div>
       </div>
+    </div>
+
+    <div class="filter-actions">
+      <b-field>
+        <b-select
+          placeholder="Add Intent"
+          expanded>
+          <option value="diagnosis">diagnosis</option>
+          <option value="treatment">treatment</option>
+          <option value="know_symptoms">Know_symptoms</option>
+          <option value="service_points">Service_points</option>
+        </b-select>
+      </b-field>
+
+      <b-field>
+        <b-select
+          placeholder="Add Entity"
+          expanded>
+          <option value="flint">Flint</option>
+          <option value="silver">Silver</option>
+        </b-select>
+      </b-field>
+
+      <div>
+        <button type="" class="btn btn-secondary">Publish</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -58,6 +54,7 @@ export default {
 
 <style lang="scss" scoped>
 @import '../../assets/scss/_colors';
+
 .filter-bot {
   width: 100%;
   background-color: #F4F4F4;
@@ -65,39 +62,61 @@ export default {
   padding: 10px 30px 20px;
   border-radius: 0 0 2px 2px;
   flex-direction: column;
-
-  input[type="text"] {
-    padding: 0;
-    height: 34px;
-    border: none;
-  }
+  margin-bottom: 30px;
 
   ::selection {
     background: $primary-color;
     color: #fff;
   }
 }
+.filter-bot__text {
+  width: 100%;
+  position: relative;
+}
 
 //--------------------
 // Hight Light Text
 //--------------------
+$font-size: 14px;
+
+.ant-input {
+  padding: 4px 7px;
+  width: 100%;
+  height: 44px;
+  cursor: text;
+  font-size: $font-size;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 0.65);
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid transparent;
+  border-radius: 5px;
+  transition: all .3s;
+  outline: none;
+
+  &:active,
+  &:focus {
+    box-shadow: 0 2px 8px 1px rgba(0, 0, 0, 0.3);
+  }
+}
+
+.hightlight-text {
+  position: absolute;
+  top: 11px;
+  left: 6px;
+  color: transparent;
+  pointer-events: none;
+  padding: 0;
+  font-size: $font-size;
+  line-height: 1.5;
+}
+
 .entity-select {
   background-color: #F5A623;
   opacity: 1;
   border-radius: 10px;
   color: #FFF;
 }
-
-.hightlight-text {
-  position: absolute;
-  top: 0;
-  left: 0;
-  color: transparent;
-  pointer-events: none;
-  padding: 5px 8px;
-  white-space: pre;
-}
-
 
 //--------------------
 .filter-actions {
