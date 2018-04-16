f324fb22b2688fe8bf7b0d651b367a65c26e1ee5
Mac Giovanni <maclevison@gmail.com>
2018-04-16 18:24:00 -0300

    Add component Issues

---
+++ src/components/bot_informations/Issues.vue
@@ -0,0 +1,58 @@
+<template>
+  <li class="bot-summary__item">
+    <span>
+      <span class="bot-summary__item-label"><slot/> issues
+      </span><i class="material-icons">error_outline</i>
+    </span>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    count: {
+      type: Number,
+      required: true
+    },
+    status: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/_colors";
+
+.bot-summary__item {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  margin-left: 30px;
+
+  .material-icons {
+    margin-left: 10px;
+    font-size: 16px;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    color: $dark-grey;
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+      color: $primary-color;
+    }
+  }
+
+  @media (max-width: 992px) {
+    margin-left: 10px;
+    .material-icons {
+      margin-left: 4px;
+    }
+  }
+}
+</style>
