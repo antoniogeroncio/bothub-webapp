c4875b63887f436a00a3db731dc96d568031042e
Mac Giovanni <maclevison@gmail.com>
2018-04-17 09:38:31 -0300

    Add component FlagItem

---
+++ src/components/shared/FlagItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="flags-item">
+    <img
+      v-bind:src="'../static/images/flags/' + flagName + '.png'"
+      v-bind:alt="'Flag '+ flagName">
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'FlagItem',
+  props: ['flagName']
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/shared/Flags.vue
+++ src/components/shared/Flags.vue
@@ -1,14 +1,55 @@
 <template>
-  <li class="flags-item">
-    <img
-      v-bind:src="'../static/images/flags/' + flagName + '.png'"
-      v-bind:alt="'Flag '+ flagName">
-  </li>
+  <ul class="card-bot__flags">
+    <flag-item flag-name="EN"/>
+    <flag-item flag-name="ES"/>
+    <flag-item flag-name="PT"/>
+    <flag-item flag-name="IT"/>
+    <flag-item flag-name="NL"/>
+    <flag-item flag-name="FR"/>
+    <flag-item flag-name="DE"/>
+  </ul>
 </template>
 
 <script>
+import FlagItem from './FlagItem'
 export default {
   name: 'Flags',
-  props: ['flagName']
+  components: {
+    FlagItem
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.card-bot__flags {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+
+  @media (max-width: 768px) {
+    background-color: #fff;
+    width: 100%;
+    margin-top: 7px;
+  }
+
+  @media (max-width: 400px) {
+    width: auto;
+  }
+
+  .flags-item {
+    width: 15px;
+    margin: 0 6px 0 0;
+
+    &:last-child {
+       margin-right: 0;
+     }
+
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
