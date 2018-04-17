e968f7dbaa398cb5b44445a506aeef9d6cea58ab
Mac Giovanni <maclevison@gmail.com>
2018-04-17 09:37:00 -0300

    Add component Stars

--- src/components/bot_informations/Stars.vue
+++ src/components/bot_informations/Stars.vue
@@ -1,6 +1,6 @@
 <template>
   <li class="bot-summary__item">
-    <span>
+    <span class="bot-summary__content">
       <span class="bot-summary__item-label"><slot/> stars</span>
       <i class="material-icons">grade</i>
     </span>
@@ -9,49 +9,6 @@
 
 <script>
 export default {
-  props: {
-    count: {
-      type: Number,
-      required: true
-    },
-    status: {
-      type: Boolean,
-      required: true
-    }
-  }
+  name: 'Stars'
 }
 </script>
-
-<style lang="scss">
-@import "../../assets/scss/_colors";
-  .bot-summary__item {
-    display: flex;
-    align-items: center;
-    font-size: 14px;
-    margin-left: 30px;
-
-    .material-icons {
-      margin-left: 10px;
-      font-size: 16px;
-    }
-
-    a {
-      display: flex;
-      align-items: center;
-      color: $dark-grey;
-      text-decoration: none;
-      transition: color 0.2s ease-in-out;
-
-      &:hover {
-        color: $primary-color;
-      }
-    }
-
-    @media (max-width: 992px) {
-      margin-left: 10px;
-      .material-icons {
-        margin-left: 4px;
-      }
-    }
-  }
-</style>
